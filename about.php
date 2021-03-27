1fde32ab098bcce45d6fbb8dc5ab1689216514d8
Chuckibmk <Chuckibmk@users.norely.github.com>
2021-03-27 19:58:51 +0100

    Initial commit

---
+++ about.php
@@ -0,0 +1,222 @@
+<!DOCTYPE html>
+<html lang="zxx">
+  <head>
+    <meta charset="utf-8">
+    <title>Theone - Onepage Multipurpose Template</title>
+    <meta content="" name="description">
+    <meta content="" name="author">
+    <meta content="" name="keywords">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+    <!-- favicon -->
+    <link href="img/favicon-theone.png" rel="icon" sizes="32x32" type="image/png">
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- font themify CSS -->
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <!-- font awesome CSS -->
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+    <!-- theone CSS -->
+    <link href="css/animated-theone.css" rel="stylesheet">
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.css" rel="stylesheet">
+    <link href="css/owl.transitions.css" rel="stylesheet">
+    <link href="css/theone-style.css" rel="stylesheet">
+    <link href="css/color/blue.css"  rel="stylesheet">
+    <link href="css/queries-theone.css" media="all" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+  
+    <!-- preloader -->
+    <div class="bg-preloader"></div>
+    <div id="preloadbar"></div>
+    <div class="preloader">
+      <div class="mainpreloader">
+        <span></span>
+      </div>
+    </div>
+    <!-- preloader end -->
+    
+    
+    
+    <!-- content wraper -->
+    <div class="content-wrapper">
+    
+        <header class="init">
+            <!-- nav -->
+            <div class="navbar-default navbar-fixed-top">
+                <div class="container">
+                <div class="row">
+                
+                <!-- menu mobile display -->
+                <button class="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse">
+                <span class="icon icon-bar"></span> 
+                <span class="icon icon-bar"></span> 
+                <span class="icon icon-bar"></span></button> 
+                
+                <!-- logo --> 
+                <a class="navbar-brand" href="index.html">
+                <img class="white" alt="logo" src="img/logo.png">
+                <img class="black" alt="logo" src="img/logo-black.png">
+                </a> 
+                <!-- logo end -->
+                
+                <!-- mainmenu start -->
+                <div class="menu-init" id="main-menu">
+                    <nav id="menu-center">
+                    <ul>
+                        <li><a class="actived" href="index.html">Home</a></li>
+                        <li><a href="about.php">About</a></li>
+                        <li><a href="index.html#contact">Contact</a></li>
+                    </ul> 
+                    </nav>
+                </div>
+                <!-- mainmenu end -->
+                
+                </div>
+                </div>
+                <!-- container -->
+            </div>
+            <!-- nav end -->
+        </header>
+
+        <!-- subheader -->
+        <section id="subheader" class="parallax-window" data-parallax="scroll" data-image-src="img/bg-subheader.jpg">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                    <h1>
+                        About Us
+                    </h1>
+                    </div>
+
+                    <div class="col-md-12">
+                    <ul class="subdetail">
+                        <li>
+                        <a href="index.html">Home</a>
+                        </li>
+
+                        <li class="sep">/
+                        </li>
+
+                        <li>About us
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- about -->    
+        <section id="about" class="white-bg no-padding">
+            <div class="container-fluid">
+                <div class="row equal">
+                
+                    <div class="col-lg-6 hidden-md hidden-sm hidden-xs">
+                        <div class="img-content onStep" data-animation="fadeInUp" data-time="300"> <img class="img-responsive" alt="img" src="img/tal-about.png"></div>
+                    </div>
+                
+                    <div class="col-lg-6 p-90">
+                            <div class="sp-padding onStep" data-animation="fadeInUp" data-time="600">
+                            
+                                <h2 class="pers-heading">
+                                    Why Choose Us
+                                    <span class="devider-heading-left"></span>
+                                </h2>           
+
+                                <p class="pers-content">
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi delectus quibusdam neque aperiam. Placeat quo nostrum perspiciatis in unde eaque, iusto reprehenderit, sequi fugiat dolor accusamus officia id, ullam eveniet.
+                                </p>
+                            
+                            <div class="clearfix space-double"></div>
+                            
+                            <div id="mainskill">
+                                    <div class="skillbar clearfix " data-percent="80%">
+                                        <div class="skillbar-title"><span>HTML5</span></div>
+                                        <div class="skillbar-bar"></div>
+                                        <div class="skill-bar-percent">20%</div>
+                                    </div> 
+
+                                    <div class="skillbar clearfix " data-percent="85%">
+                                        <div class="skillbar-title"><span>CSS3</span></div>
+                                        <div class="skillbar-bar"></div>
+                                        <div class="skill-bar-percent">25%</div>
+                                    </div>
+
+                                    <div class="skillbar clearfix " data-percent="70%">
+                                        <div class="skillbar-title"><span>jQuery</span></div>
+                                        <div class="skillbar-bar"></div>
+                                        <div class="skill-bar-percent">50%</div>
+                                    </div>
+
+                                    <div class="skillbar clearfix " data-percent="60%">
+                                        <div class="skillbar-title"><span>PHP</span></div>
+                                        <div class="skillbar-bar"></div>
+                                        <div class="skill-bar-percent">40%</div>
+                                    </div>
+
+                                    <div class="skillbar clearfix " data-percent="75%">
+                                        <div class="skillbar-title"><span>Wordpress</span></div>
+                                        <div class="skillbar-bar"></div>
+                                        <div class="skill-bar-percent">75%</div>
+                                    </div> 
+
+                                    <div class="skillbar clearfix " data-percent="90%">
+                                        <div class="skillbar-title"><span>Photoshop</span></div>
+                                        <div class="skillbar-bar"></div>
+                                        <div class="skill-bar-percent">70%</div>
+                                    </div>
+                                </div>
+                            
+                        </div>
+                        <div class="clearfix"></div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+        <!-- about end -->
+
+        <!-- footer -->
+        <footer class="black text-center">
+            <div class="container">
+                <div class="row">
+            
+                    <div class="col-md-12">
+                        <div class="social-icons">
+                        <a href="#"><span class="ti-facebook"></span></a>
+                        <a href="#"><span class="ti-dribbble"></span></a>
+                        <a href="#"><span class="ti-twitter"></span></a>
+                        <a  href="#"><span class="ti-instagram"></span></a>
+                        <a href="#"><span class="ti-linkedin"></span></a>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-12">
+                    <span class="color">&copy; Theone 2017.</span> all reserved design by on3step
+                    </div>
+
+                
+                </div>
+            </div>
+        </footer>
+        <!-- footer end -->
+      
+        <!-- ScrolltoTop -->
+        <div id="totop" class="init">
+            <span class="ti-angle-up"></span>
+        </div>
+      
+    </div>
+    <!-- content wraper end -->
+
+   <!-- plugin JS -->
+    <script src="plugin/pluginstheone.js" type="text/javascript"></script> 
+    <script src="plugin/parallax.min.js" type="text/javascript"></script> 
+    <script src="plugin/sticky.js" type="text/javascript"></script> 
+    <!-- Theone JS -->
+    <script src="js/theone.js" type="text/javascript"></script>
+    <script src="js/nav.js" type="text/javascript"></script>
+    
+  
+  </body>
+</html>
---
+++ contact.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html>
+<html lang="zxx">
+  <head>
+    <meta charset="utf-8">
+    <title>Theone - Onepage Multipurpose Template</title>
+    <meta content="" name="description">
+    <meta content="" name="author">
+    <meta content="" name="keywords">
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+    <!-- favicon -->
+    <link href="img/favicon-theone.png" rel="icon" sizes="32x32" type="image/png">
+    <!-- Bootstrap CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!-- font themify CSS -->
+    <link rel="stylesheet" href="css/themify-icons.css">
+    <!-- font awesome CSS -->
+    <link href="font-awesome/css/font-awesome.css" rel="stylesheet">
+    <!-- theone CSS -->
+    <link href="css/animated-theone.css" rel="stylesheet">
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.css" rel="stylesheet">
+    <link href="css/owl.transitions.css" rel="stylesheet">
+    <link href="css/theone-style.css" rel="stylesheet">
+    <link href="css/color/blue.css"  rel="stylesheet">
+    <link href="css/queries-theone.css" media="all" rel="stylesheet" type="text/css">
+  </head>
+  <body>
+  
+    <!-- preloader -->
+    <div class="bg-preloader"></div>
+    <div id="preloadbar"></div>
+    <div class="preloader">
+      <div class="mainpreloader">
+        <span></span>
+      </div>
+    </div>
+    <!-- preloader end -->
+    
+    
+    
+    <!-- content wraper -->
+    <div class="content-wrapper">
+    
+    <header class="init">
+      <!-- nav -->
+      <div class="navbar-default navbar-fixed-top">
+        <div class="container">
+        <div class="row">
+        
+          <!-- menu mobile display -->
+          <button class="navbar-toggle" data-target=".navbar-collapse" data-toggle="collapse">
+           <span class="icon icon-bar"></span> 
+           <span class="icon icon-bar"></span> 
+           <span class="icon icon-bar"></span></button> 
+          
+          <!-- logo --> 
+          <a class="navbar-brand" href="index.html">
+          <img class="white" alt="logo" src="img/logo.png">
+          <img class="black" alt="logo" src="img/logo-black.png">
+          </a> 
+          <!-- logo end -->
+          
+          <!-- mainmenu start -->
+          <div class="menu-init" id="main-menu">
+            <nav id="menu-center">
+              <ul>
+                <li><a class="actived" href="index.html#home">Home</a></li>
+                <li><a href="index.html#about">About</a></li>
+                <li><a href="index.html#projects">Gallery</a></li>
+                <li><a href="index.html#">Blog <i class="fa fa-angle-down"></i></a>
+                  <ul>
+                    <li><a href="blog.html">Blog Single</a></li>
+                    <li><a href="blog-post.html">Blog Post</a></li>
+                  </ul>
+                </li>
+                <li><a href="index.html#contact">Contact</a></li>
+              </ul> 
+            </nav>
+          </div>
+          <!-- mainmenu end -->
+          
+        </div>
+        </div>
+        <!-- container -->
+      </div>
+      <!-- nav end -->
+    </header>
+
+<!-- contact -->
+<section aria-label="map" class="subfooter" id="contact" style="background-image:url(img/bg-contact.jpg)">
+        <div class="container">
+          <div class="row">
+          
+          <div class="onStep" data-animation="fadeInUp" data-time="300">
+          
+          <div class="col-md-12">
+           <h2 class="devider-heading">Get In Touch</h2>
+
+           <p class="content-max-1 text-center">
+           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolores ptio neque obcaecati! Incidunt temporibus blanditiis eveniet co
+           </p>
+           
+           <address class="inline-block">
+                  <span><i class="ti-mobile"></i>+6221 000 888 999</span> 
+                  <span><i class="ti-email"></i><a href="mailto:companyname@gmail.com">companyname@gmail.com</a></span> 
+                  <span><i class="ti-map-alt"></i>London NW1 6XE, UK 10903</span> 
+           </address>
+           
+           </div>
+           
+          <!-- spacer -->
+          <div class="space-single"></div>
+          
+          <div class="no-gutter">
+          
+            <div class="col-md-6">  
+              <div class="box">
+                <div id="mapinline-1"></div>
+              </div>
+            </div>
+          
+            <div class="col-md-6">
+              <div class="box p-60">
+                <h3>
+                  Leave Message
+                </h3>
+                
+                <p>
+                Lorem sit amet, consectetur adipiscing elit nam lobortis eget nibh
+                </p>
+
+                <form action="#" class="row" id="form-contact" method="post" name="form-contact">
+                  <input id="name-contact" name="name" placeholder="your name" type="text"> 
+                  <input id="email-contact" name="email" placeholder="your e-mail" type="text"> 
+                  <textarea cols="50" id="message-contact" name="message" placeholder="your enquiry" rows="4"></textarea>
+                  <div class="success" id="mail_success">Thank you. Your message has been sent</div>
+                  <div class="error" id="mail_failed"> Error, email not sent</div>
+                  <button class="btn" id="send-contact" type="submit">Sent Now</button>
+                </form>
+             </div>
+           </div> 
+
+          </div>
+          
+          </div>
+           
+        </div>
+        </div>
+        </section>
+        <!-- contact end -->
+
+        <!-- footer -->
+      <footer class="black text-center">
+        <div class="container">
+          <div class="row">
+          
+          <div class="col-md-12">
+                <div class="social-icons">
+                  <a href="#"><span class="ti-facebook"></span></a>
+                  <a href="#"><span class="ti-dribbble"></span></a>
+                  <a href="#"><span class="ti-twitter"></span></a>
+                  <a  href="#"><span class="ti-instagram"></span></a>
+                  <a href="#"><span class="ti-linkedin"></span></a>
+                </div>
+            </div>
+            
+            <div class="col-md-12">
+              <span class="color">&copy; Theone 2017.</span> all reserved design by on3step
+            </div>
+
+            
+          </div>
+        </div>
+      </footer>
+      <!-- footer end -->
+      
+      <!-- ScrolltoTop -->
+      <div id="totop" class="init">
+        <span class="ti-angle-up"></span>
+      </div>
+      
+    </div>
+    <!-- content wraper end -->
+
+   <!-- plugin JS -->
+    <script src="plugin/pluginstheone.js" type="text/javascript"></script> 
+    <script src="plugin/parallax.min.js" type="text/javascript"></script> 
+    <script src="plugin/sticky.js" type="text/javascript"></script> 
+    <!-- Theone JS -->
+    <script src="js/theone.js" type="text/javascript"></script>
+    <script src="js/nav.js" type="text/javascript"></script>
+    
+  
+  </body>
+</html>
